8e79714454ccbcd990b152ba288d3ea14aea76fe
ManWen512 <waiyan55512@gmail.com>
2025-07-10 20:46:20 +0700

    style: update dark theme styling for dialog boxes
    - Improved background and text contrast in dark mode dialogs

--- app/balance/balancedialog/page.js
+++ app/balance/balancedialog/page.js
@@ -2,14 +2,35 @@
 
 import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addAccount, editAccount } from "@/redux/slices/balanceSlice";
 import TextField from "@mui/material/TextField";
 import { showSnackbar } from "@/redux/slices/snackBarSlice";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { selectTheme } from "@/redux/selectors/settingsSelectors";
+
+const darkMuiTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      paper: "#1f2937", // Tailwind gray-800
+      default: "#111827", // Tailwind gray-900
+    },
+    text: {
+      primary: "#fff",
+    },
+  },
+});
+
+const lightMuiTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
 
 export default function BalanceDialogPage({ accId, name, onClose }) {
   const dispatch = useDispatch();
-
+  const theme = useSelector(selectTheme);
   const [accData, setAccData] = useState({
     name: name || "",
   });
@@ -71,27 +92,31 @@ export default function BalanceDialogPage({ accId, name, onClose }) {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-teal-100 p-6 rounded-lg shadow-lg mx-5 w-full sm:w-1/3">
-        <h2 className="text-xl font-semibold mb-5">
+      <div className=" p-6 rounded-lg shadow-lg mx-5 w-full sm:w-1/3  bg-gradient-to-br from-teal-50 to-white dark:bg-gradient-to-br dark:from-teal-900 dark:to-black">
+        <h2 className="text-xl font-semibold mb-5 dark:text-white">
           {accId ? "Edit Account" : "Add New Account"}
         </h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <TextField
-              id="outlined-basic"
-              label="Account Name"
-              variant="outlined"
-              name="name"
-              value={accData.name}
-              onChange={handleChange}
-              className="w-full p-2 border rounded "
-              placeholder="Enter account name"
-              required
-              sx={sx}
-            />
+            <ThemeProvider
+              theme={theme === "dark" ? darkMuiTheme : lightMuiTheme}
+            >
+              <TextField
+                id="outlined-basic"
+                label="Account Name"
+                variant="outlined"
+                name="name"
+                value={accData.name}
+                onChange={handleChange}
+                className="w-full p-2 border rounded dark:text-white"
+                placeholder="Enter account name"
+                required
+                sx={sx}
+              />
+            </ThemeProvider>
           </div>
 
-          <div className="flex justify-end gap-3">
+          <div className="flex justify-end gap-3 dark:text-white">
             <button type="button" onClick={onClose}>
               Cancel
             </button>
--- app/category/categorydialog/page.js
+++ app/category/categorydialog/page.js
@@ -1,16 +1,35 @@
 "use client";
 
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useDispatch,useSelector } from "react-redux";
 import { addCategory, editCategory } from "@/redux/slices/categorySlice";
 import TextField from "@mui/material/TextField";
 import { showSnackbar } from "@/redux/slices/snackBarSlice";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { selectTheme } from "@/redux/selectors/settingsSelectors";
+
+const darkMuiTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      paper: "#1f2937", // Tailwind gray-800
+      default: "#111827", // Tailwind gray-900
+    },
+    text: {
+      primary: "#fff",
+    },
+  },
+});
+
+const lightMuiTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
 
 export default function CategoryDialog({ catId, name, onClose }) {
   const dispatch = useDispatch();
-
-
+  const theme = useSelector(selectTheme)
   const [catData, setCatData] = useState({
     name: name || "",
   });
@@ -26,13 +45,22 @@ export default function CategoryDialog({ catId, name, onClose }) {
     e.preventDefault();
     if (catId) {
       dispatch(editCategory({ id: catId, ...catData }));
-      dispatch(showSnackbar({message:"Category updated successfully!", severity:"success"}));
+      dispatch(
+        showSnackbar({
+          message: "Category updated successfully!",
+          severity: "success",
+        })
+      );
       onClose();
     } else {
       dispatch(addCategory(catData));
-      dispatch(showSnackbar({message:"Category added successfully!", severity:"success"}));
+      dispatch(
+        showSnackbar({
+          message: "Category added successfully!",
+          severity: "success",
+        })
+      );
       onClose();
-      
     }
   };
 
@@ -64,27 +92,31 @@ export default function CategoryDialog({ catId, name, onClose }) {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-teal-100 p-6 rounded-lg shadow-lg mx-5 w-full sm:w-1/3">
-        <h2 className="text-xl font-semibold mb-5">
+      <div className=" p-6 rounded-lg shadow-lg mx-5 w-full sm:w-1/3 bg-gradient-to-br from-teal-50 to-white dark:bg-gradient-to-br dark:from-teal-900 dark:to-black">
+        <h2 className="text-xl font-semibold mb-5 dark:text-white">
           {catId ? "Edit Category" : "Add New Category"}
         </h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <TextField
-              id="outlined-basic"
-              label="Category Name"
-              variant="outlined"
-              name="name"
-              value={catData.name}
-              onChange={handleChange}
-              className="w-full p-2 border rounded "
-              placeholder="Enter Category Name"
-              required
-              sx={sx}
-            />
+            <ThemeProvider
+              theme={theme === "dark" ? darkMuiTheme : lightMuiTheme}
+            >
+              <TextField
+                id="outlined-basic"
+                label="Category Name"
+                variant="outlined"
+                name="name"
+                value={catData.name}
+                onChange={handleChange}
+                className="w-full p-2 border rounded dark:text-white"
+                placeholder="Enter Category Name"
+                required
+                sx={sx}
+              />
+            </ThemeProvider>
           </div>
 
-          <div className="flex justify-end gap-3">
+          <div className="flex justify-end gap-3 dark:text-white">
             <button type="button" onClick={onClose}>
               Cancel
             </button>
